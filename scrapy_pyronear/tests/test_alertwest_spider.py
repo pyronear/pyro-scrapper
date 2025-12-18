f499ca3687035cfc891f6e21b695d9e1970dd564
Clara Vega <clara.vega@student-cs.fr>
2025-12-18 17:04:23 +0100

    improve tests

--- scrapy_pyronear/tests/test_alertwest_spider.py
+++ scrapy_pyronear/tests/test_alertwest_spider.py
@@ -64,6 +64,9 @@ def test_alertwest_spider_parse():
                 "data": [
                     {"p": 120, "lmt": "1600000", "id": "CAM001", "img": "image.jpg", "cn": "Camera 1", "pn": "Provider A"},
                     {"p": 45, "lmt": "1700000", "id": "CAM002", "img": "photo.png", "cn": "Camera 2", "pn": "Provider B"},
+                    {"p": 46, "lmt": "1700000", "id": "CAM002", "img": "photo.png", "cn": "Camera 3", "pn": "Provider B DOT"},
+                    {"p": 47, "lmt": "1700000", "id": "CAM002", "img": "photo.png", "cn": "Camera 4 Thermal", "pn": "Provider B"},
+                    {"p": 47, "lmt": "1700000", "id": "CAM002", "img": "", "cn": "Camera 4 Thermal", "pn": "Provider B"}
                 ]
             }
         }
